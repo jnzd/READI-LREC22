5dad9b270996cb2139cec440f261de9562570e5f
Jonas Degelo <jonas.degelo@gmail.com>
2022-11-28 14:16:21 +0100

    fixed prdictor return value for BERT

--- readability/models/bert.py
+++ readability/models/bert.py
@@ -90,7 +90,7 @@ def classify_corpus_BERT(corpus, model_name = "camembert-base", percent_train=90
     results_summary.append(cm_init)
     r = models.compute_evaluation_metrics(results_summary[0],round=2, data_name="", class_names=corpus_label_names)
     models.pp.pprint(r)
-    return ktrain.get_predictor(learner.model, preproc), r
+    return model, r
     # return r
 
 def getTransformer(model_name, x_train, y_train, x_test, y_test, class_names, batch_size=6):
